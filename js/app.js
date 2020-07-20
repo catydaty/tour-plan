60708deae8aec1700ff8e5d55175cf79a333fd8b
catydaty <catawasu9@gmail.com>
2020-07-20 14:10:39 +0300

    create: added swiper-slider on hotel section

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+  effect: 'coverflow',
+});
