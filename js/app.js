77eabae1a6396a6bc1f38c3e0177354be498e01e
catydaty <catawasu9@gmail.com>
2020-07-21 17:52:17 +0300

    fix: fixbag-script

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ var mySwiper = new Swiper('.swiper-container', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.slider-button__next',
+    prevEl: '.slider-button__prev',
   },
   effect: 'coverflow',
   keyboard: {
