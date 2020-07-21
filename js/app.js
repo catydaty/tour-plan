f867941f61adbaeff7e5d6c85d045374079ea840
catydaty <catawasu9@gmail.com>
2020-07-21 17:41:23 +0300

    fix:  replaced app.js

--- js/app.js
+++ js/app.js
@@ -13,3 +13,21 @@ var mySwiper = new Swiper('.swiper-container', {
     onlyInViewport: false,
   },
 });
+
+var myMap;
+ymaps.ready(init);
+function init() {
+  myMap = new ymaps.Map('map', {
+    center: [7.838196, 98.298813], // Hilton
+    zoom: 15
+  });
+
+  var myGeoObject = new ymaps.GeoObject({
+    geometry: {
+      type: "Point",
+      coordinates: [7.838196, 98.298813]
+    },
+  });
+
+  myMap.geoObjects.add(myGeoObject);
+};
