46bbde16ad28368f692baacf846e79ebb6500e0b
catydaty <catawasu9@gmail.com>
2020-07-31 00:34:58 +0300

    create: added 2 form

--- send.php
+++ send.php
@@ -5,27 +5,6 @@ require 'phpmailer/SMTP.php';
 require 'phpmailer/Exception.php';
 
 
-// Переменные, которые отправляет пользователь
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-// $email = $_POST['sendemail'];
-
-// Отправка емайла
-// $title_2 = "Новая подписка из Best Tour Plan";
-// $body_2 = "
-// <h2>Новая подписка пользователя</h2>
-// <b>Почта:</b> $email
-// ";
-
-// Формирование самого письма
-$title = "Новое обращение Best Tour Plan";
-$body = "
-<h2>Новое обращение</h2>
-<b>Имя:</br> $name<br>
-<b>Телефон:</br> $phone<br><br>
-<b>Сообщение:</b><br>$message
-";
 
 
 // Настройки PHPMailer
@@ -41,7 +20,7 @@ try {
     // Настройки вашей почты
     $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
     $mail->Username   = 'ijojo1703@gmail.com'; // Логин на почте
-    $mail->Password   = 'kweezE$4'; // Пароль на почте
+    $mail->Password   = 'kweeze4rezzota2A'; // Пароль на почте
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;
     $mail->setFrom('ijojo1703@gmail.com', 'Имя отправителя'); // Адрес самой почты и имя отправителя
@@ -49,15 +28,44 @@ try {
     // Получатель письма
     $mail->addAddress('catawasu9@gmail.com');  
 
-    // Отправка почты
-    // $mail->isHTML(true);
-    // $mail->Subject = $title_2;
-    // $mail->Body = $body_2;  
+    if (isset($_POST['submit_1'])){
+    $email = $_POST['email'];
     
-    // Отправка сообщения
+    // отправка почты
+    $title_1 = "Новое обращение Best Tour Plan";
+    $body_1 = "
+    <h2>New subscriber</h2>
+    <b>Message:</b><br>$email
+    ";
+
     $mail->isHTML(true);
-    $mail->Subject = $title;
-    $mail->Body = $body; 
+    $mail->Subject = $title_1;
+    $mail->Body = $body_1;
+
+    header('Location: thank_you_subscribe.html');
+    }
+
+    if(isset($_POST['submit_2'])) {
+    // Переменные, которые отправляет пользователь
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    // Формирование самого письма
+    $title_2 = "Новое обращение Best Tour Plan";
+    $body_2 = "
+    <h2>New appeal</h2>
+    <b>Name:</br> $name<br>
+    <b>Phone:</br> $phone<br><br>
+    <b>Message:</b><br>$message
+    ";
+    
+    $mail->isHTML(true);
+    $mail->Subject = $title_2;
+    $mail->Body = $body_2;
+    
+    header('Location: thank_you_feedback.html');
+    }
 
 // Проверяем отравленность сообщения
 if ($mail->send()) {$result = "success";} 
@@ -69,6 +77,5 @@ else {$result = "error";}
 }
 
 // Отображение результата
-echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+// echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
 
-// header('Location: thankyou.html');
