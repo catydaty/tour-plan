fae31b4acace487ff2c23446ccff6511499ac3f7
catydaty <catawasu9@gmail.com>
2020-07-31 16:00:00 +0300

    create: added modal

--- js/app.js
+++ js/app.js
@@ -1,63 +1,87 @@
-/* добавляем слайдеры */
-var hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
+  /* добавляем слайдеры */
+  var hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button_next',
-    prevEl: '.hotel-slider__button_prev',
-  },
-  effect: 'coverflow',
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
-var reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button_next',
-    prevEl: '.reviews-slider__button_prev',
-  },
-  effect: 'slide',
-  autoHeight: true,
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
-/* добавляет метку на карту */
-var myMap;
-
-ymaps.ready(init);
-
-function init() {
-  myMap = new ymaps.Map('map', {
-    center: [7.838196, 98.298813], // Hilton
-    zoom: 15
-  });
-
-  var myGeoObject = new ymaps.GeoObject({
-    geometry: {
-      type: "Point",
-      coordinates: [7.838196, 98.298813]
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button_next',
+      prevEl: '.hotel-slider__button_prev',
+    },
+    effect: 'coverflow',
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
     },
   });
+  var reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
 
-  myMap.geoObjects.add(myGeoObject);
-};
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button_next',
+      prevEl: '.reviews-slider__button_prev',
+    },
+    effect: 'slide',
+    autoHeight: true,
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+  /* добавляет метку на карту */
+  var myMap;
 
-/* добавляем эффект паралакса */
-$('.parallax-window').parallax({ imageSrc: 'img/newsletter-bg.jpeg' });
+  ymaps.ready(init);
 
-// задаем свойства для мобильного меню
-let menuButton = document.querySelector('.menu-button');
-menuButton.addEventListener('click', function () {
-  document
-    .querySelector('.navbar-bottom')
-    .classList.toggle('navbar-bottom__visible');
-}) 
+  function init() {
+    myMap = new ymaps.Map('map', {
+      center: [7.838196, 98.298813], // Hilton
+      zoom: 15
+    });
+
+    var myGeoObject = new ymaps.GeoObject({
+      geometry: {
+        type: "Point",
+        coordinates: [7.838196, 98.298813]
+      },
+    });
+
+    myMap.geoObjects.add(myGeoObject);
+  };
+
+  /* добавляем эффект паралакса */
+  $('.parallax-window').parallax({ imageSrc: 'img/newsletter-bg.jpeg' });
+
+  // задаем свойства для мобильного меню
+  let menuButton = $('.menu-button');
+  menuButton.on('click', function () {
+    $('.navbar-bottom').toggle('navbar-bottom__visible');
+  });
+
+  // добавляем модальное окно
+  let modalButton = $('[data-toggle="modal"]');
+  let closeModalButton = $('.modal__close');
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+
+  function openModal() {
+    let modalOverlay = $('.modal__overlay');
+    let modalDialog = $('.modal__dialog');
+    modalOverlay.addClass('modal__overlay__visible');
+    modalDialog.addClass('modal__dialog__visible');
+  };
+  function closeModal(event) {
+    event.preventDefault();
+    let modalOverlay = $('.modal__overlay');
+    let modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay__visible');
+    modalDialog.removeClass('modal__dialog__visible');
+  };
+
+  document.addEventListener('keydown', function (e) {
+    if (e.keyCode === 27) document.getElementById('modal_id').hidden = 1;
+  });
+});
